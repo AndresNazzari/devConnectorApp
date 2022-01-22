fe09c52491e522b8c6ee573d4f2ae761d32f9452
Andres Nazzari <andres.nazzari@valtech.com>
2022-01-22 20:18:36 -0300

    bug en api para eliminar usuario

--- client/src/actions/profiles.js
+++ client/src/actions/profiles.js
@@ -226,7 +226,7 @@ export const deleteAccount = () => async (dispatch) => {
 			await axios.delete(`/api/profile`);
 			dispatch({ type: CLEAR_PROFILE });
 			dispatch({ type: ACCOUNT_DELETED });
-			dispatch(setAlert('Education Your account has been permanently deleted'));
+			dispatch(setAlert('Your account has been permanently deleted'));
 		} catch (err) {
 			const errors = err.response.data.errors;
 			if (errors) {
